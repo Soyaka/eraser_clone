b8b881c94d375613cb666b85b2c77fd1c5361eac
soyaka <soyaka@soyaka.soyaka>
2024-03-18 14:00:31 +0000

    last update

--- app/_components/AdBanner.tsx
+++ app/_components/AdBanner.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
 
-function AdBanner({props}:any) {
-    useEffect(() => {
-        try {
-          ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
-        } catch (err) {
-          console.log(err);
-        }
-      }, []);
+// function AdBanner({props}:any) {
+//     useEffect(() => {
+//         try {
+//           ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       }, []);
     
-      return (
-        <ins
-          className="adsbygoogle adbanner-customize"
-          style={{
-            display: 'block',
-            overflow: 'hidden',
-          }}
-          data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}
-          {...props}
-        />
-      );
+//       return (
+//         <ins
+//           className="adsbygoogle adbanner-customize"
+//           style={{
+//             display: 'block',
+//             overflow: 'hidden',
+//           }}
+//           data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}
+//           {...props}
+//         />
+//       );
     
-}
+// }
 
-export default AdBanner
+// export default AdBanner
